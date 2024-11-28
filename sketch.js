de4f740bcdac506783984f62e5ebdebcc6deda2c
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-11-28 09:49:42 +0000

    add cols & rows

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+const canvasWidth = 600;
+const canvasHeight = 300;
+
+const numberOfRows = canvasHeight / 2;
+const numberOfCols = canvasWidth / 2;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(canvasWidth, canvasHeight);
 }
   
 function draw() {
     background(220);
+
+    for(let row = 0; row < numberOfRows; row++) {
+      for(let col = 0; col < numberOfCols; col++) {
+
+      }
+    }
 }
