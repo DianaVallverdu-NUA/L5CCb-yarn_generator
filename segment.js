e116327c7c59582e4a5a2cb13f2f9a8e1860d8a9
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-11-28 11:35:05 +0000

    refactor segment class

--- segment.js
+++ segment.js
@@ -2,25 +2,27 @@ class Segment {
   constructor(vertical) {
     this.vertical = vertical;
 
+    //calculate init and length according to direction
     if (vertical) {
       this.init = random(width);
       this.length = min(random(width - this.init), 150);
     }
 
     if (!vertical) {
-      // calculate space it occupies
       this.init = random(height);
       this.length = min(random(height - this.init), 100);
     }
 
+    //calculate end point
     this.end = this.init + this.length;
   }
 
+  //draw current segment
   draw() {
-    for (let i = 0; i < this.length / threadSpace; i++) {
-      if (this.vertical)
-        drawVerticalLine(this.init + threadSpace * i, 0, height);
-      else drawHorizontalLine(this.init + threadSpace * i, 0, width);
+    //loop through length with a step of threadSpace
+    for (let i = this.init; i < this.end; i += threadSpace) {
+      if (this.vertical) drawVerticalLine(i, 0, height);
+      else drawHorizontalLine(i, 0, width);
     }
   }
 
